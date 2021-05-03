3cc73671f4d19b38b94994397a690bfe6237e4b4
Johannes Posse <johannesposse@gmail.com>
2021-05-03 09:33:32 +0200

    asd

--- 6. Web Development/ButtonsJS/button.js
+++ 6. Web Development/ButtonsJS/button.js
@@ -1,6 +1,14 @@
 function buttonFunc(){
     console.log("mafk");
-
-    
     document.getElementById("buttonDemo").innerHTML +="MAFK";
 }
+
+let num = 10;
+document.getElementById("mafkOnClick").addEventListener("mousemove", function(){
+    document.getElementById("mafkOnClick").style.paddingTop ="";
+    document.getElementById("mafkOnClick").style.paddingTop +=num +"px";
+    document.getElementById("mafkOnClick").style.paddingLeft ="";
+    document.getElementById("mafkOnClick").style.paddingLeft +=num +"px";
+    num = num + 10;
+    // alert(num);
+});
