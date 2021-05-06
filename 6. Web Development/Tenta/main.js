6a472fa19f576735ee4442ab33f32876d87b266e
Johannes Posse <johannesposse@gmail.com>
2021-05-06 17:00:33 +0200

    asdas

--- 6. Web Development/Tenta/main.js
+++ 6. Web Development/Tenta/main.js
@@ -1,25 +1,30 @@
 let search ="";
+// let perPage = "per_page=9"
 const protcol = "https://"
 const urlName = 'www.flickr.com/'
 const path = "services/"
 const query = "rest/?method=flickr.photos.search&"
 const apiKey = "api_key=8ed52e76b5c4ab1bec78c9e13fbd3f60"
-const query2 = "&sort=relevance&per_page=9&format=json&nojsoncallback=1"
+const query2 = "&sort=relevance&"
+const query3 = "&format=json&nojsoncallback=1"
 
 
 const btn = document.querySelector(".btn")
 
 btn.addEventListener("click", async function(){
     let search = document.querySelector("#search").value
-    
+    let perPage = "per_page=" + document.querySelector("#perPage").value
+    document
+    console.log(perPage)
+        
     if(search == "")
     {
-        alert("FÅR INTE VARA TOM")
+        alert("please enter something in the searchbox")
     }
     else
     {
         let searchPath ="&text=" + search
-        let url = `${protcol}${urlName}${path}${query}${apiKey}${searchPath}${query2}`
+        let url = `${protcol}${urlName}${path}${query}${apiKey}${searchPath}${query2}${perPage}${query3}`
         const response = await fetch(url)
         const data = await response.json()
         generateIMG(data.photos.photo);
@@ -37,7 +42,7 @@ function generateIMG(data){
         const serverID = data[i].server;
         const id = data[i].id;
         const secret = data[i].secret;
-        const imgUrl = `https://live.staticflickr.com/${serverID}/${id}_${secret}_z.jpg`
+        const imgUrl = `https://live.staticflickr.com/${serverID}/${id}_${secret}_c.jpg`
         const addImg = document.querySelector(".images");
 
         if(i == 0){
@@ -54,8 +59,10 @@ function generateIMG(data){
     }
     const changeImg = document.querySelectorAll(".newImg").forEach(x => {
             x.addEventListener("click", function(){
+                //k
             let top = document.querySelector(".topImg").style.backgroundImage
             let bot = x.style.backgroundImage
+            console.log(bot)
             document.querySelector(".topImg").style.backgroundImage = bot
             x.style.backgroundImage = top
         })
