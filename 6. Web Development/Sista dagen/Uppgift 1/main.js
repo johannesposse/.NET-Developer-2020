a503fa7f6f696c16ea7cee3c7069da200c03113d
Johannes Posse <johannesposse@gmail.com>
2021-05-06 10:56:45 +0200

    updates

--- 6. Web Development/Sista dagen/Uppgift 1/main.js
+++ 6. Web Development/Sista dagen/Uppgift 1/main.js
@@ -3,15 +3,21 @@ btn.addEventListener("click",async function(){
     const respons = await fetch("https://swapi.dev/api/people/1/")
     const data = await respons.json()
 
-    let keys =["name", "eye-color", "height", "mass"]
 
     const nameP = document.querySelector(".name")
     const eyeColorP = document.querySelector(".eye-color")
     const heightP = document.querySelector(".height")
     const massP = document.querySelector(".mass")
+    const filmList = document.querySelector(".films")
 
     nameP.innerText = data.name
     eyeColorP.innerText =data.eye_color
     heightP.innerText = data.height
     massP.innerText = data.mass
+
+    for(let film of data.films){
+        const li  = document.createElement("li")
+        li.innerText = film
+        filmList.append(li)
+    }
 })
