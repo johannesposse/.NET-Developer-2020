fe975596474f77b6bd86f42651a4072366570d75
Johannes Posse <johannesposse@gmail.com>
2021-05-19 15:53:28 +0200

    up

--- 7. ASP.NET/freeCodeCamp/BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
+++ 7. ASP.NET/freeCodeCamp/BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -2,15 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly DbCon dbCon;
+
+        public IndexModel(DbCon db)
         {
+            dbCon = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await dbCon.Book.ToListAsync();
         }
     }
 }
