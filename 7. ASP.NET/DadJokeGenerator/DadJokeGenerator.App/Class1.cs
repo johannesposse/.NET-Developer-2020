eed1127801455c2c353200ac9973ecd564d87a49
Johannes Posse <johannesposse@gmail.com>
2021-09-07 09:52:37 +0200

    dadjokegenerator

---
+++ 7. ASP.NET/DadJokeGenerator/DadJokeGenerator.App/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DadJokeGenerator.App
+{
+    public class Class1
+    {
+    }
+}
---
+++ 7. ASP.NET/DadJokeGenerator/DadJokeGenerator.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DadJokeGenerator.Data
+{
+    public class Class1
+    {
+    }
+}
