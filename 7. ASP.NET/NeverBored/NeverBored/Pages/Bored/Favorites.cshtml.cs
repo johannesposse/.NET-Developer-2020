d6aba2a257dbc54aa3d93f236ce5691c3220ba13
Johannes Posse <johannesposse@gmail.com>
2021-09-17 11:37:28 +0200

    fixed delety

--- 7. ASP.NET/NeverBored/NeverBored/Pages/Bored/Favorites.cshtml.cs
+++ 7. ASP.NET/NeverBored/NeverBored/Pages/Bored/Favorites.cshtml.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NeverBored.Model;
+using Newtonsoft.Json;
 
 namespace NeverBored.Pages.Bored
 {
@@ -11,6 +14,28 @@ namespace NeverBored.Pages.Bored
     {
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPostDelete(string key)
+        {
+
+            for(int i = 0; i < IndexModel.Boreds.Count(); i++)
+            {
+                if(IndexModel.Boreds[i].Key == key)
+                {
+                    IndexModel.Boreds.RemoveAt(i);
+                }
+            }
+
+            string _boreds = JsonConvert.SerializeObject(IndexModel.Boreds);
+            HttpContext.Session.SetString("boreds", _boreds);
+
+            return RedirectToPage("/Bored/Favorites");
         }
     }
 }
