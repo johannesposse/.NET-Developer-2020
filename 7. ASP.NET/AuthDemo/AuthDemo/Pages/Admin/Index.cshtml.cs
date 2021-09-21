dc36b047a06fecff441dfdfdcd11e395d0fdcb5c
Johannes Posse <johannesposse@gmail.com>
2021-09-21 11:00:56 +0200

    signout

--- 7. ASP.NET/AuthDemo/AuthDemo/Pages/Admin/Index.cshtml.cs
+++ 7. ASP.NET/AuthDemo/AuthDemo/Pages/Admin/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,21 @@ namespace AuthDemo.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public IndexModel(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            signInManager.SignOutAsync();
+
+            return RedirectToPage("/Index");
         }
     }
 }
